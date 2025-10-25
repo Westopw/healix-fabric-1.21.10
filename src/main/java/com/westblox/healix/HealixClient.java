09f664693d40aae2deb5e997c2157ff231bd58d3
Westopw <westopw.bon@gmail.com>
2025-10-25 18:12:32 +0200

    Initial commit

---
+++ src/main/java/com/westblox/healix/HealixClient.java
@@ -0,0 +1,10 @@
+package com.westblox.healix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HealixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/westblox/healix/HealixDataGenerator.java
@@ -0,0 +1,11 @@
+package com.westblox.healix;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HealixDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
